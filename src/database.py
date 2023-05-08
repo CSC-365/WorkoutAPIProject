bb404d832eda0feb4718d84aeb186a1ab338a2e7
nickambrose7 <nickambrose7@gmail.com>
2023-05-07 22:11:28 -0700

    pull kennys stuff

--- src/database.py
+++ src/database.py
@@ -21,13 +21,13 @@ def database_connection_url():
 # creating a new DB engine based on our connection string
 engine = sqlalchemy.create_engine(database_connection_url(), echo=True, future=True)
 
-supabase_api_key = os.environ.get("SUPABASE_API_KEY")
-supabase_url = os.environ.get("SUPABASE_URL")
+# supabase_api_key = os.environ.get("SUPABASE_API_KEY")
+# supabase_url = os.environ.get("SUPABASE_URL")
 
-if supabase_api_key is None or supabase_url is None:
-    raise Exception(
-        "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
-    )
+# if supabase_api_key is None or supabase_url is None:
+#     raise Exception(
+#         "You must set the SUPABASE_API_KEY and SUPABASE_URL environment variables."
+#     )
 
 
 # # THIS CODE IS ALL FOR READING FROM CSVS DELETE DELETE DELETE
