2b1cd0d8d5201cb71de7a881b6211e94de5feb60
nickambrose7 <nickambrose7@gmail.com>
2023-05-08 18:59:30 -0700

    finished all testing

--- test/test_goals.py
+++ test/test_goals.py
@@ -5,7 +5,7 @@ client = TestClient(app)
 
 def test_create_goal():
     response = client.post("/goals/", json={
-        "user_id": 2,
+        "user_id": 4,
         "type_id": 0,
         "target_weight": 150
     })
@@ -13,7 +13,7 @@ def test_create_goal():
     # no other way to test since we don't have a get goal endpoint
 def test_create_goal2():
     response = client.post("/goals/", json={
-        "user_id": 1,
+        "user_id": 5,
         "type_id": 0,
         "target_weight": 150
     })
